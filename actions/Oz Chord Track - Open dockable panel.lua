2ea5b708886c1e8973fca9fc8986566dbd7dd61c
ozone1979 <ozone1979@gmail.com>
2026-03-10 20:50:47 -0400

    Add toggle state for dockable panel action

--- actions/Oz Chord Track - Open dockable panel.lua
+++ actions/Oz Chord Track - Open dockable panel.lua
@@ -1,3 +1,19 @@
 local script_path = debug.getinfo(1, "S").source:match("@?(.*[\\/])")
 local core = dofile(script_path .. "Oz Chord Track Core.lua")
+
+local _, _, section_id, command_id = reaper.get_action_context()
+
+local function set_action_toggle(state)
+	if not section_id or not command_id or command_id == 0 then
+		return
+	end
+	reaper.SetToggleCommandState(section_id, command_id, state and 1 or 0)
+	reaper.RefreshToolbar2(section_id, command_id)
+end
+
+set_action_toggle(true)
+reaper.atexit(function()
+	set_action_toggle(false)
+end)
+
 core.run_dockable_panel()
